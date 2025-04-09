efa966f072dd322c1611fbdc3d4139d5529760e9
shelendra <sheltech@shelendras-MacBook-Air.local>
2025-04-09 15:48:19 +0530

    homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import MainBanner from '../components/MainBanner'
+import Categories from '../components/Categories'
+import BestSeller from '../components/BestSeller'
+import BottomBanner from '../components/BottomBanner'
+import NewsLatter from '../components/NewsLatter'
+
+
+const Home = () => {
+  return (
+    <div className='mt-10'>
+      <MainBanner />
+      <Categories />
+      <BestSeller />
+      <BottomBanner />
+      <NewsLatter />
+      
+    </div>
+  )
+}
+
+export default Home
